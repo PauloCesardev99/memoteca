4db23665d9d292b3d6eb96cf058aa03932373da4
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-02-01 06:32:43 -0300

    first commit

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+
+export class CriarPensamentoComponent {
+  
+  pensamento={
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento(){
+    window.alert("Novo Pensamento criado")
+
+  }
+
+  cancelarPensamento(){
+    window.alert("Cancelando Pensamento")
+  }
+
+}
