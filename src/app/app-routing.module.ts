67df23573c65b7f74eb896c3c5a1ee43d9d124ef
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-02-02 08:04:22 -0300

    rotas criadas e implementadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '', //rota do 4200
+    redirectTo: 'listarPensamento', //padrão pro componente listarPensamento
+    pathMatch: 'full' // considera todo a url
+  },
+
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrl: './listar-pensamento.component.css'
+})
+export class ListarPensamentoComponent {
+
+}
